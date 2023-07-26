06e4b0bbcdb743f7a664569408f91401772fc831
Wallmonger <nicolas.berrier360@gmail.com>
2023-07-26 19:11:08 +0200

    Refactor and useful keyboards shortcut

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // ALT + ENTER : Extract method to create function based on highlighted text
+    // Right click and go to definition send to the function
+    // ALT + ARROW UP : Put line in top
+
+
     private float xInput;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
     private Rigidbody2D rb;
     private Animator anim;
-    [SerializeField] private bool isMoving;
     
 
     void Start()
@@ -21,22 +25,36 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        Movement();
+        CheckInput();
+        AnimatorControllers();
+    }
 
-
-
-        if (Input.GetButtonDown("Jump")) 
-        {
-            rb.velocity += new Vector2(rb.velocity.x, jumpForce);
-        }
-
+    private void CheckInput()
+    {
         xInput = Input.GetAxisRaw("Horizontal");
-        Debug.Log(xInput);
 
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void Movement()
+    {
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.velocity += new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void AnimatorControllers () 
+    {
         // If player is moving, we set the boolean value to trigger the animation
-        isMoving = rb.velocity.x != 0;
+        bool isMoving = rb.velocity.x != 0;
 
         anim.SetBool("isMoving", isMoving);
-        
     }
 }
